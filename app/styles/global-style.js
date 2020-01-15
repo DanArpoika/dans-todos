40464b76f8c3790e94c159258b2bfbc32022cb0b
Brett Smith <hello@thebrettsmith.com>
2020-01-15 11:41:25 -0600

    Initial commit

---
+++ app/styles/global-style.js
@@ -0,0 +1,50 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+
+  :root {
+    --container-width: 1240px;
+  }
+
+  *,
+  *::before,
+  *::after { box-sizing: border-box; }
+
+  body {
+    margin: 0;
+    font-family: "hevletica", sans-serif;
+    font-size: 1rem;
+    font-weight: 400;
+    line-height: 1.5;
+    color: ${props => props.theme.black};
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    margin: 0;
+    font-weight: 600;
+    line-height: 1;
+    color: inherit;
+  }
+
+  img { max-width: 100%; }
+
+  figure { margin: 0; }
+
+  form { position: relative; }
+
+  button,
+  a {
+    outline: none;
+
+    &::-moz-focus-inner { border: 0; }
+
+    &:not(:disabled) { cursor: pointer; }
+  }
+`;
+
+export default GlobalStyle;
