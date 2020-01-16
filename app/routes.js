f6946dc057e87f8d62e8ce94a2e457cae912089a
Brett Smith <hello@thebrettsmith.com>
2020-01-16 08:53:46 -0600

    remove old routes file

--- app/routes.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable no-multi-assign */
-const routes = module.exports = require('next-routes')();
-
-routes
-  .add('index', '/')
-  .add('posts', '/posts')
-  .add('posts/single', '/posts/:id');
